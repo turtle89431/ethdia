bea5f4fd01072c94c666c1ce8f4c9596628a9360
Walter Winser <turtle89431@gmail.com>
2019-07-28 13:15:33 -0700

    added a temp file

---
+++ example.js
@@ -0,0 +1,38 @@
+let fetch = require('fetch');
+let getdata = url =>
+	new Promise((res, rej) => {
+		fetch.fetchUrl(url, {}, (e, meta, d) => {
+			res(d.toString());
+		});
+	});
+let clean = arr => {};
+function diff_minutes(dt1) {
+	let dt2 = new Date();
+	var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+	diff /= 60;
+	let min = Math.abs(Math.floor(diff));
+	let sec = Math.round((diff - min) * 60);
+	return `${min}:${sec}`;
+}
+let run = async () => {
+	let x = await getdata('https://api.hitbtc.com/api/2/public/trades/ETHDAI');
+	let data = JSON.parse(x);
+	//console.log(data)
+	let output = [];
+	let s = 'buy';
+	await data.forEach(d => {
+		/**
+         * price: '207.720',
+        quantity: '0.0010',
+        side: 'buy',
+         */
+		let { price, quantity, side, timestamp } = d;
+		if (s != side) {
+			let ts = new Date(timestamp);
+			output.push({ price, quantity, side, time: diff_minutes(ts) });
+			s = side;
+		}
+	});
+	return output;
+};
+run().then(o => console.log(o.reverse()));
